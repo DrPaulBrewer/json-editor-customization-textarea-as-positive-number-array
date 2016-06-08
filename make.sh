8aadaa89e3e829ac39d66246c64280a633a3c3f7
Dr Paul Brewer <drpaulbrewer@gmail.com>
2016-06-07 22:59:05 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 npm update
 browserify main.js -o bundle.js
-INSTALL=/var/web/192.168.1.10/editor/
-mkdir -p $INSTALL
-cp index.html $INSTALL 
-cp bundle.js $INSTALL
+echo "edit this make.sh file to copy the finished files: index.html and bundle.js --  to your web server"
+# here is what I use to copy it to my server. 
+# replace this with your own deployment code
+# INSTALL=/var/web/192.168.1.10/editor/
+# mkdir -p $INSTALL
+# cp index.html $INSTALL 
+# cp bundle.js $INSTALL
